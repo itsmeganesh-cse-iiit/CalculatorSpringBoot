af9b613b762302f2c508335685c11412cbdd26a4
Ganesh Koilada <itsmeganeshcse@gmail.com>
2019-11-26 14:37:56 +0530

    @Autowired , @Scope and @Qualifier annotations usage

--- src/main/java/com/example/demo/CalculatorApplication.java
+++ src/main/java/com/example/demo/CalculatorApplication.java
@@ -2,20 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.ConfigurableApplicationContext;;
 
 @SpringBootApplication
-@Scope(value="prototype")
+
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context =SpringApplication.run(CalculatorApplication.class, args);
 		Student s = context.getBean(Student.class);
 		s.show();
-		
-		Student s1 = context.getBean(Student.class);
-		s1.show();
 		
 		System.out.println("Spring boot Application.");
 	}
--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+
+//@Scope(value="prototype")
 public class Student {
 	
 	private String name;
 	private String grade;
 	private int marks;
+	@Autowired
+	@Qualifier("BookObj")
+	private Book book;
 	
 	public Student() {
 		super();
@@ -32,8 +40,17 @@ public class Student {
 		this.marks = marks;
 	}
 	
+	
+	
+	public Book getBook() {
+		return book;
+	}
+	public void setBook(Book book) {
+		this.book = book;
+	}
 	public void show() {
-		System.out.println("hello");
+		System.out.println("Student entered...!");
+		book.read();
 		
 	}
 	
